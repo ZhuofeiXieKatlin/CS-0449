c466dfd6d31e581d6f6b110a0fb283f06be1dd61
ZhuofeiXieKatlin <zhx34@pitt.edu>
2019-03-23 16:17:42 -0400

    lab1/

---
+++ lab1/lab1.c
@@ -0,0 +1,12 @@
+
+// Zhuofei Xie (zhx34)
+#include<stdio.h>
+
+ int main() {
+     int x = 10; 
+     printf("size of (x) = %d\n", (int)sizeof(x)); 
+     printf("size of (int) = %d\n", (int)sizeof(int)); 
+     printf("Hello World!\n");
+     return 0;
+ }
+
